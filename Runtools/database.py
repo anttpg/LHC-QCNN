94142137a0731bd21180f506b90b59ab6cc2d321
Ben Dobbins <bdobbins@colgate.edu>
2024-03-06 22:20:30 -0500

    Implement working database, allow controller to update database with results and params during every run

--- Runtools/database.py
+++ Runtools/database.py
@@ -11,8 +11,17 @@ from functools import partial
 class Database:
 
     def __init__(self, filepath):
-        self.path = filepath
-        self.circuit_list = #FIGURE OUT?
+        # self.circuit_list = #FIGURE OUT?
+        self.conn = sqlite3.connect(filepath)
+        self.cursor = self.conn.cursor()
+
+
+
+    # Close and save database
+    def close(self):
+        self.conn.close()
+
+
 
     def plot_datapoints(self):
         signal_dict, background_dict, files_used = lqm.load_data(
@@ -30,12 +39,166 @@ class Database:
 
     # MAKE A DICTIONARY of (circuit_id -> [plot, func] list) that will be checked every time the callback called.
     plots = []
-    
+
+
+
+
+    def select_spsas(self, spsa_alpha, spsa_gamma, spsa_c, spsa_A, spsa_a1):
+        """
+        This function selects the id of the given spsa parameters from the database. Or None if the parameters do not exist. 
+        """
+        self.cursor.execute("SELECT id FROM spsas WHERE spsa_alpha = ? AND spsa_gamma = ? AND spsa_c = ? AND spsa_A = ? AND spsa_a1 = ?", 
+                            (spsa_alpha, spsa_gamma, spsa_c, spsa_A, spsa_a1))
+        # Will be none if the parameters do not exist, otherwise will be the id of the parameters inside a list
+        spsa_id = self.cursor.fetchone()
+        return spsa_id if spsa_id is None else spsa_id[0]
+
+
+
+
+
+    def select_feature_keys(self, n_qubits, feature_keys):
+        """
+        This function selects the id of the given feature keys from the database. Or None if the keys do not exist. 
+        """
+        message = "SELECT id FROM feature_keys WHERE n_qubits = ? AND "
+        # Add a ? for each feature key
+        for i in range(1, len(feature_keys) + 1):
+            message += f"feature{i} = ? AND "
+        # Remove the last " AND " from the message
+        message = message[:-5]
+
+        # Execute the message with the n_qubits and feature keys as parameters
+        self.cursor.execute(message, (n_qubits, *feature_keys))
+
+        # Will be none if the parameters do not exist, otherwise will be the id of the parameters inside a list
+        feature_keys_id = self.cursor.fetchone()
+        return feature_keys_id if feature_keys_id is None else feature_keys_id[0]
+
+
+
+
+    def select_data_sizes(self, train_data_size, valid_data_size, test_data_size):
+        """
+        This function selects the id of the given data sizes from the database. Or None if the sizes do not exist. 
+        """
+        self.cursor.execute("SELECT id FROM data_sizes WHERE train_data_size = ? AND valid_data_size = ? AND test_data_size = ?", 
+                            (train_data_size, valid_data_size, test_data_size))
+        # Will be none if the parameters do not exist, otherwise will be the id of the parameters inside a list
+        data_sizes_id = self.cursor.fetchone()
+        return data_sizes_id if data_sizes_id is None else data_sizes_id[0]
+
+
+
+
+    def select_misc_params(self, n_epochs, batch_size, num_layers, obs, is_local_simulator, use_pca, seed):
+        """
+        This function selects the id of the given misc parameters from the database. Or None if the parameters do not exist. 
+        """
+        self.cursor.execute("SELECT id FROM misc_params WHERE n_epochs = ? AND batch_size = ? AND num_layers = ? AND obs = ? AND is_local_simulator = ? AND use_pca = ? AND seed = ?", 
+                            (n_epochs, batch_size, num_layers, obs, is_local_simulator, use_pca, seed))
+        # Will be none if the parameters do not exist, otherwise will be the id of the parameters inside a list
+        misc_params_id = self.cursor.fetchone()
+        return misc_params_id if misc_params_id is None else misc_params_id[0]
+
+
+
+
+
+    def create_circuit_entry(self, params):
+        """
+        Create a new entry in the circuits table with the given parameters. Return the id of the new entry as the unique id for the current circuit.
+        """
+
+        # Check if the spsa params already exist in the table
+        spsas_id = self.select_spsas(params.spsa_alpha, params.spsa_gamma, params.spsa_c, params.spsa_A, params.spsa_a1)
+        # If the spsa params do not exist in the table, add them and get the new id
+        if spsas_id is None:
+            self.cursor.execute("INSERT INTO spsas (spsa_alpha, spsa_gamma, spsa_c, spsa_A, spsa_a1) VALUES (?, ?, ?, ?, ?)", 
+                                (params.spsa_alpha, params.spsa_gamma, params.spsa_c, params.spsa_A, params.spsa_a1))
+            # Get new id (can only use this for tables with autoincrementing ids)
+            spsas_id = self.cursor.lastrowid
+
+
+        # Check if the feature keys already exist in the table
+        feature_keys_id = self.select_feature_keys(params.n_qubits, params.training_feature_keys)
+        # If the feature keys do not exist in the table, add them and get the new id
+        if feature_keys_id is None:
+            message = "INSERT INTO feature_keys (n_qubits, "
+
+            # Add a ? for each feature key
+            for i in range(1, len(params.training_feature_keys) + 1):
+                message += f"feature{i}, "
+            # Remove the last ", " from the message
+            message = message[:-2]
+
+            # First question mark for n_qubits, then one for each feature key
+            message += ") VALUES (?, "
+            # Add a ? for each feature key
+            for i in range(1, len(params.training_feature_keys)):
+                message += "?, "
+            message += "?)"
+
+            # Execute the message with the n_qubits and feature keys as parameters
+            self.cursor.execute(message, (params.n_qubits, *params.training_feature_keys))
+            # Get new id
+            feature_keys_id = self.cursor.lastrowid
+        
+
+        # Check if the data sizes already exist in the table
+        data_sizes_id = self.select_data_sizes(params.train_data_size, params.valid_data_size, params.test_data_size)
+        # If the data sizes do not exist in the table, add them and get the new id
+        if data_sizes_id is None:
+            self.cursor.execute("INSERT INTO data_sizes (train_data_size, valid_data_size, test_data_size) VALUES (?, ?, ?)", 
+                                (params.train_data_size, params.valid_data_size, params.test_data_size))
+            data_sizes_id = self.cursor.lastrowid
+
+
+        # Check if the misc params already exist in the table
+        misc_params_id = self.select_misc_params(params.n_epochs, params.batch_size, params.num_layers, params.obs_text, params.is_local_simulator, params.use_pca, params.seed)
+        # If the misc params do not exist in the table, add them and get the new id
+        if misc_params_id is None:
+            self.cursor.execute("INSERT INTO misc_params (n_epochs, batch_size, num_layers, obs, is_local_simulator, use_pca, seed) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                                (params.n_epochs, params.batch_size, params.num_layers, params.obs_text, params.is_local_simulator, params.use_pca, params.seed))
+            misc_params_id = self.cursor.lastrowid
+
+
+        # Add the new circuit entry to the outputs table
+        self.cursor.execute("INSERT INTO outputs (spsas_id, feature_keys_id, data_sizes_id, misc_params_id) VALUES (?, ?, ?, ?)",
+                            (spsas_id, feature_keys_id, data_sizes_id, misc_params_id))
+
+        # Commit the changes to the database
+        self.conn.commit()
+
+        # Return the id of the new entry in outputs (main) table
+        # The entry with this id will later be updated with the results of the run
+        return self.cursor.lastrowid
+
+
+
+
 
     # Callback function to be called by controller every time it completes a runner (ie like run_one completes), signal to the database that new 
     # Data has appeared, allowing us to update the graphs, AND  SOMEHOW SAVE DATA
-    def update_callback(circuit_id, new_data):
-        return None
+    def update_callback(self, circuit_id, new_data):
+        # Add the new valid loss to the database with each entry corresponding to the new id (all entries with this id represent the losses for this run)
+        for entry in new_data.valid_loss:
+            self.cursor.execute("INSERT INTO valid_loss (circuit_id, valid_loss) VALUES (?, ?)", (circuit_id, entry))
+
+        # Add the new test data to the database with each entry corresponding to the new id (all entries with this id represent the test data for this run)
+        for i in range(len(new_data.test_labels)):
+            self.cursor.execute("INSERT INTO test_data (circuit_id, test_label, test_prob, test_pred) VALUES (?, ?, ?, ?)", 
+                                (circuit_id, int(new_data.test_labels[i]), new_data.test_prob[i], new_data.test_pred[i]))
+
+        # Update the outputs table with the new results
+        self.cursor.execute("UPDATE outputs SET cost = ?, test_accuracy = ?, test_data_id = ?, valid_loss_id = ?, run_time = ? WHERE id = ?",
+                            (new_data.cost, new_data.test_accuracy, circuit_id, circuit_id, new_data.run_time, circuit_id))
+
+        # Commit the changes to the database
+        self.conn.commit()
+
+
+
 
 
     # Given a circuit_id a name of a key in the database, and CONDITIONS that the data must pass, return an array of that data. 
@@ -43,7 +206,7 @@ class Database:
 
     # Ideally, we would have some way to pass over conditons that must be met for the data to be included in the results passed back, EX
     # Conditions = order most recent results, first 100 results, results must not == 0 
-    def get_data(circuit_id, conditions, key_name):
+    def get_data(self, circuit_id, conditions, key_name):
         return None
     
 
@@ -52,26 +215,28 @@ class Database:
     # X_DATA, Y_DATA are TUPLES, with the data (condition, key_name)
 
     # Save this plot as another entry under the circuit_id dictionary, as a tuple of the (plot reference, and partial function)
-    def plot(circuit_id, x_data, y_data):
+    def plot(self, circuit_id, x_data, y_data):
 
-        partial(get_data, 'yes')
+        # partial(get_data, 'yes')
         return None
     
     # Given a list of circuit IDs and a variables for the X/Y list of (CONDITION, STRING) type, generate a plot for each circuit, return list of PLOTS
-    def create_plots(ids, xs, ys):
+    def create_plots(self, ids, xs, ys):
         return None
     
     # Given a plot, UPDATE the X/Y values based on new data
-    def update_XY(plot, x_data, y_data):
+    def update_XY(self, plot, x_data, y_data):
         return None
     
     # Given a list of circuit IDs, update each plots data corresponding to that circuit ID
-    def update_plots(ids):
+    def update_plots(self, ids):
+        pass
 
 
 
     # Given a plot, generate a single image with some helpful result name, and return its filepath
-    def generate_image():
+    def generate_image(self):
+        pass
         
 
     # Given a plot, generate a GIF of the change rel
@@ -187,70 +352,70 @@ class Database:
 
 class Train_Test_Data:
 
-        def __init__(self, params):
-            self.params = params
+    def __init__(self, params):
+        self.params = params
 
-            self.n_signal_events = None
-            self.n_background_events = None
-            
-            self.train_features = None
-            self.train_labels = None
+        self.n_signal_events = None
+        self.n_background_events = None
+        
+        self.train_features = None
+        self.train_labels = None
 
-            self.valid_features = None
-            self.valid_labels = None
+        self.valid_features = None
+        self.valid_labels = None
 
-            self.rest_features = None
-            self.rest_labels = None
+        self.rest_features = None
+        self.rest_labels = None
 
-            self.test_features = None
-            self.test_labels = None
-
-
-        # TODO: update to use signal / background processes
-        def tts_preprocess(self, signal, background):
-            # load data from files
-            signal_dict, background_dict, files_used = lqm.load_data(
-                self.params.signals_folder, self.params.backgrounds_folder, self.params.training_feature_keys
-            )
-
-            # formats data for input into vqc
-            features, labels = lqm.format_data(signal_dict, background_dict)
-
-            self.n_signal_events = (labels == 1).sum()
-            self.n_background_events = (labels == 0).sum()
-
-            features_signal = features[(labels==1)]
-            features_background = features[(labels==0)]
-
-            np.random.shuffle(features_signal)
-            np.random.shuffle(features_background)
-
-            features = np.concatenate((features_signal[:self.params.half_datasize], features_background[:self.params.half_datasize]))
-            # labels = np.array([1]*half_datasize + [0]*half_datasize, requires_grad=False)
-            labels = np.array([1]*self.params.half_datasize + [0]*self.params.half_datasize)
-
-            train_features1, rest_features1, self.train_labels, self.rest_labels = train_test_split(
-                features,
-                labels,
-                train_size=self.params.train_data_size,
-                test_size=self.params.test_data_size + self.params.valid_data_size,
-                random_state=self.params.seed,
-                stratify=labels
-            )
-
-            # preprocess data (rescaling)
-            self.train_features, self.rest_features = lqm.preprocess_data(
-                train_features1, rest_features1, self.params.use_pca, self.params.num_features, self.params.seed
-            )
+        self.test_features = None
+        self.test_labels = None
 
 
-            self.valid_features, self.test_features, self.valid_labels, self.test_labels = train_test_split(
-                self.rest_features,
-                self.rest_labels,
-                train_size=self.params.valid_data_size,
-                test_size = self.params.test_data_size,
-                random_state=self.params.seed,
-                stratify=self.rest_labels
-            )
+    # TODO: update to use signal / background processes
+    def tts_preprocess(self, signal, background):
+        # load data from files
+        signal_dict, background_dict, files_used = lqm.load_data(
+            self.params.signals_folder, self.params.backgrounds_folder, self.params.training_feature_keys
+        )
+
+        # formats data for input into vqc
+        features, labels = lqm.format_data(signal_dict, background_dict)
+
+        self.n_signal_events = (labels == 1).sum()
+        self.n_background_events = (labels == 0).sum()
+
+        features_signal = features[(labels==1)]
+        features_background = features[(labels==0)]
+
+        np.random.shuffle(features_signal)
+        np.random.shuffle(features_background)
+
+        features = np.concatenate((features_signal[:self.params.half_datasize], features_background[:self.params.half_datasize]))
+        # labels = np.array([1]*half_datasize + [0]*half_datasize, requires_grad=False)
+        labels = np.array([1]*self.params.half_datasize + [0]*self.params.half_datasize)
+
+        train_features1, rest_features1, self.train_labels, self.rest_labels = train_test_split(
+            features,
+            labels,
+            train_size=self.params.train_data_size,
+            test_size=self.params.test_data_size + self.params.valid_data_size,
+            random_state=self.params.seed,
+            stratify=labels
+        )
+
+        # preprocess data (rescaling)
+        self.train_features, self.rest_features = lqm.preprocess_data(
+            train_features1, rest_features1, self.params.use_pca, self.params.num_features, self.params.seed
+        )
+
+
+        self.valid_features, self.test_features, self.valid_labels, self.test_labels = train_test_split(
+            self.rest_features,
+            self.rest_labels,
+            train_size=self.params.valid_data_size,
+            test_size = self.params.test_data_size,
+            random_state=self.params.seed,
+            stratify=self.rest_labels
+        )
 
 
--- Runtools/main.py
+++ Runtools/main.py
@@ -4,9 +4,11 @@ import os
 import itertools
 import time
 from controller import Controller
+from database import Database
+
+DATABASE_PATH = "./database/database.db"
 
 #from multiprocessing import Pool TODO Implement multiprocessing 
-# TODO Add outputs folder to gitignore
 
 # This is a temporary function for creating the parameter configurations
 # Once the interface is implemented, we can do this a different way
@@ -32,28 +34,30 @@ def create_param_configs():
     a (float) – A hyperparameter expected to be small in noisy situations, its value could be picked using A, α and ^g0(^θ0). For more details, see Spall (1998b).
     """
 
-    for i in range(len(combinations)):
-        for j in range(runs_per_permutation):
-            param_dict[j + (i*runs_per_permutation)] = {
-                "n_qubits": 3,
-                "n_layers": 5,
-                "batch_size": 2,
-                "n_epochs": 1,
-                "train_data_size": 80,
-                "valid_data_size": 40,
-                "test_data_size": 80,
-                "is_local_simulator": True,
-                "spsa_alpha": 0.5 + random.uniform(-0.015, 0.015) if j != 0 else 0.5,
-                "spsa_gamma": 0.101 + random.uniform(-0.01, 0.01) if j != 0 else 0.101,
-                "spsa_c": 0.2 + random.uniform(-0.01, 0.015) if j != 0 else 0.2,
-                "spsa_A": 2 + random.uniform(-0.025, 0.025) if j != 0 else 2,
-                "spsa_a1": 0.2 + random.uniform(-0.01, 0.015) if j != 0 else 0.2,
-                "use_pca": False,
-                "seed": 123,
-                "training_feature_keys": combinations[i],
-                "num_features": 3,
-                "obs": "XXI"
-            }
+    # for i in range(len(combinations)):
+        # for j in range(runs_per_permutation):
+            # param_dict[j + (i*runs_per_permutation)] = {
+    for i in range(3):
+        param_dict[i] = {
+        "n_qubits": 3,
+        "n_layers": 5,
+        "batch_size": 2,
+        "n_epochs": 1,
+        "train_data_size": 80,
+        "valid_data_size": 40,
+        "test_data_size": 80,
+        "is_local_simulator": True,
+        "spsa_alpha": 0.5 + random.uniform(-0.015, 0.015) if i == 1 else 0.5,
+        "spsa_gamma": 0.101 + random.uniform(-0.01, 0.01) if i == 1 else 0.101,
+        "spsa_c": 0.2 + random.uniform(-0.01, 0.015) if i == 1 else 0.2,
+        "spsa_A": 2 + random.uniform(-0.025, 0.025) if i == 1 else 2,
+        "spsa_a1": 0.2 + random.uniform(-0.01, 0.015) if i == 1 else 0.2,
+        "use_pca": False,
+        "seed": 123,
+        "training_feature_keys": combinations[i if i != 1 else 0],
+        "num_features": 3,
+        "obs": "XXI"
+    }
 
     with open("params.json", "w") as f:
         json.dump({run_id: params for run_id, params in param_dict.items()}, f, indent=4)
@@ -67,8 +71,8 @@ def optimize_hyperparams(param_dict ):
 
 def main():
     create_param_configs()
-    # database = Database() # Feed reference to controller 1
-    c = Controller()
+    database = Database(DATABASE_PATH)
+    c = Controller(database)
 
     start = time.time()
 
@@ -77,9 +81,12 @@ def main():
 
         # Create a runner for each set of parameters in the json file
         for run_id, params in param_dict.items():
-            c.create_runner(params, run_id)
+            circuit_id = c.create_runner(params)
 
     c.run_all()
+
+    # This is important, otherwise the database will not be saved
+    database.close()
 
     # It is fine to do this because the params are saved in the output files
     os.remove("params.json")
